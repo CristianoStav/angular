da10254411b01105ff75fcfed0f7c1262e918f90
cristiano.stavarengo <cristiano.stavarengo@7comm.com.br>
2019-05-24 12:24:38 -0300

    [ANG-2] Criação da pagina de erro not-found.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app.routing.module';
     BrowserModule,
     PhotosModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PhotosListComponent } from './photos/photo-list/photos-list.component';
 import { PhotoAddComponent } from './photos/photo-add/photo-add.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
     { path: 'user/flavio', component: PhotosListComponent },
-    { path: 'add/user', component: PhotoAddComponent }
+    { path: 'add/user', component: PhotoAddComponent },
+    { path: '**', component: NotFoundComponent }
 ]
 
 @NgModule({
