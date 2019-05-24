f1d6862db170e00a0bff85107737c8339e0d1248
cristiano.stavarengo <cristiano.stavarengo@7comm.com.br>
2019-05-24 16:37:32 -0300

    [ANG-4] Grid de imagens.

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, SimpleChange, SimpleChanges } from '@angular/core';
+import { Photo } from '../../photo/photo';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnInit {
+
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for (let i = 0; i < photos.length; i += 3) {
+      newRows.push(photos.slice(i, i + 3));
+    }
+    return newRows;
+  }
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from '@angular/common';
 import { PhotosListComponent } from './photo-list/photos-list.component';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoAddComponent } from './photo-add/photo-add.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
-    declarations: [PhotoComponent, PhotosListComponent, PhotoAddComponent],
+    declarations: [PhotoComponent, PhotosListComponent, PhotoAddComponent, PhotosComponent],
     imports: [HttpClientModule, CommonModule]
 })
 
